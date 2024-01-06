4aa46a888b9a7c99bf9074efeac2a406495fd6a1
tiyol <tiyol@LAPTOP-6IPT3PBJ>
2024-01-06 23:51:47 +0300

    Proje dosyası ekle.

---
+++ odev18/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ornek223
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("İsmi Giriniz: ");
+            string isim = Console.ReadLine();
+
+            char[] karakterler = isim.ToCharArray(); // String'i karakter dizisine çevirme
+
+            bool[] D = new bool[karakterler.Length];
+            Random rastgele = new Random();
+
+            Console.Write("Rastgele Karıştırılmış İsim: ");
+
+            for (int a = 0; a < karakterler.Length; a++)
+            {
+                int sayi;
+                do
+                {
+                    sayi = rastgele.Next(karakterler.Length);
+                } while (D[sayi]);
+
+                D[sayi] = true;
+                Console.Write(karakterler[sayi]);
+            }
+
+            Console.Write("\n\n\nPress any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
